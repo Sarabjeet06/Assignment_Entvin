6b6e7dbb883157e5a506a6204070f6bfd37099e0
Sarabjeet06 <sarabjeet@0601.com>
2024-10-05 18:54:56 +0530

    updating some stylings

--- src/components/CustomTable.jsx
+++ src/components/CustomTable.jsx
@@ -13,15 +13,19 @@ import Medicine_3 from '../assets/Medicine_3.svg';
 import Medicine_4 from '../assets/Medicine_4.svg';
 import Medicine_5 from '../assets/Medicine_5.svg';
 import Medicine_6 from '../assets/Medicine_6.svg';
-import Profit_1 from '../assets/Profit_1.svg';
-import Loss_1 from '../assets/Loss_1.svg';
+import UpArrow from '../assets/UpArrow.svg';
+import DownArroww from '../assets/DownArroww.svg';
 
 // Styled table cell for customization
 const StyledTableCell = styled(TableCell)(({ theme }) => ({
   [`&.${tableCellClasses.head}`]: {
     backgroundColor: '#ffffff', // Set head cells to white
-    color: theme.palette.common.black, // Change text color to black for visibility
-    border: '1px solid #ddd', // Add border for head cells
+    color: 'black', // Change text color to black for visibility
+    fontWeight: '500',
+    fontSize: '15px',
+    border: '1px solid #ddd', // Add border for head cells\
+    padding: '12px 10px',
+    boxSizing: 'border-box'
   },
   [`&.${tableCellClasses.body}`]: {
     fontSize: 14,
@@ -45,20 +49,18 @@ const data = {
     {
       sno: {
         value: 1,
-        imgSrc: null,
-        altValue: '',
         profit: 'up',
+        percent: '+2',
       }, company: {
         imgSrc: Medicine_1,
         altValue: 'GlaxoSmithKline Pharmaceuticals',
         name: 'Shingrix',
         desc: 'Chronic rhinosinusitis with nasal polyps (CRSwNP)',
       }, revenue23: {
-        imgSrc: null,
-        altValue: '',
         value: '3,400',
         curr: ' Euro Million',
         profit: 'up',
+        percent: '+17',
       }, revenue22: {
         value: '11,789',
         curr: ' Euro Million',
@@ -73,20 +75,18 @@ const data = {
     {
       sno: {
         value: 2,
-        imgSrc: null,
-        altValue: '',
-        profit: 'up',
+        profit: null,
+        percent: null,
       }, company: {
         imgSrc: Medicine_1,
         altValue: 'GlaxoSmithKline Pharmaceuticals',
         name: 'Trelegy Ellipta',
         desc: 'HER2+ Adjuvant Breast Cancer (CompassHER2 RD)',
       }, revenue23: {
-        imgSrc: null,
-        altValue: '',
         value: '22,00',
         curr: 'Euro Million',
         profit: 'up',
+        percent: '+29',
       }, revenue22: {
         value: '1,705',
         curr: 'Euro Million',
@@ -100,20 +100,18 @@ const data = {
     }, {
       sno: {
         value: 3,
-        imgSrc: null,
-        altValue: '',
-        profit: 'up',
+        profit: null,
+        percent: null,
       }, company: {
         imgSrc: Medicine_1,
         altValue: 'GlaxoSmithKline Pharmaceuticals',
         name: 'Dovato',
         desc: 'Active immunization for the prevention of varicella in individuals 12 months of age and older',
       }, revenue23: {
-        imgSrc: null,
-        altValue: '',
         value: '12,206',
         curr: '$ Dollar Million',
         profit: 'up',
+        percent: '+33',
       }, revenue22: {
         value: '11,789',
         curr: '$ Dollar Million',
@@ -130,20 +128,18 @@ const data = {
     {
       sno: {
         value: 1,
-        imgSrc: null,
-        altValue: 'Pfizer',
-        profit: '',
+        profit: null,
+        percent: null,
       }, company: {
         imgSrc: Medicine_6,
         altValue: 'Pfizer',
         name: 'Comirnaty',
         desc: 'Active immunization to prevent COVID-19 disease caused by SARS-CoV-2 in individuals 12 years and older',
       }, revenue23: {
-        imgSrc: Loss_1,
-        altValue: 'Pfizer',
         value: '11,220',
         curr: '$ Dollar Million',
         profit: 'down',
+        percent: '-70',
       }, revenue22: {
         value: '37,806',
         curr: '$ Dollar Million',
@@ -158,20 +154,18 @@ const data = {
     {
       sno: {
         value: 2,
-        imgSrc: null,
-        altValue: 'Pfizer',
-        profit: '',
+        profit: null,
+        percent: null,
       }, company: {
         imgSrc: Medicine_6,
         altValue: 'Pfizer',
         name: 'Eliquis',
         desc: 'Secondary Stroke Prevention',
       }, revenue23: {
-        imgSrc: Profit_1,
-        altValue: 'Pfizer',
         value: '12,206',
         curr: '$ Dollar Million',
         profit: 'up',
+        percent: '+4',
       }, revenue22: {
         value: '11,789',
         curr: '$ Dollar Million',
@@ -189,9 +183,8 @@ const data = {
     {
       sno: {
         value: 1,
-        imgSrc: '',
-        altValue: '',
         profit: 'up',
+        percent: '+2',
       }, company: {
         imgSrc: Medicine_4,
         altValue: '',
@@ -199,11 +192,10 @@ const data = {
         name2: 'Eliquis',
         desc: 'Secondary Stroke Prevention',
       }, revenue23: {
-        imgSrc: '',
-        altValue: '',
         value: '12,206',
         curr: '$ Dollar Million',
         profit: 'up',
+        percent: '+4',
       }, revenue22: {
         value: '11,789',
         curr: '$ Dollar Million',
@@ -218,9 +210,8 @@ const data = {
     {
       sno: {
         value: 2,
-        imgSrc: null,
-        altValue: '',
-        profit: 'up',
+        profit: null,
+        percent: null,
       }, company: {
         imgSrc: Medicine_4,
         altValue: '',
@@ -228,11 +219,10 @@ const data = {
         name2: 'Opdivo',
         desc: 'Respiratory syncytial virus prophylaxis in adult population 18-49 years of age at increased risk',
       }, revenue23: {
-        imgSrc: null,
-        altValue: '',
         value: '12,206',
         curr: '$ Dollar Million',
         profit: 'up',
+        percent: '+9',
       }, revenue22: {
         value: '8,249',
         curr: '$ Dollar Million',
@@ -246,9 +236,8 @@ const data = {
     }, {
       sno: {
         value: 3,
-        imgSrc: null,
-        altValue: '',
-        profit: 'up',
+        profit: null,
+        percent: null,
       }, company: {
         imgSrc: Medicine_4,
         altValue: '',
@@ -256,11 +245,10 @@ const data = {
         name2: 'Orencia',
         desc: 'metastatic castration-resistant prostate cancer (mCRPC)',
       }, revenue23: {
-        imgSrc: null,
-        altValue: '',
         value: '3,601',
         curr: '$ Dollar Million',
         profit: 'up',
+        percent: '+4',
       }, revenue22: {
         value: '3,464',
         curr: '$ Dollar Million',
@@ -277,20 +265,18 @@ const data = {
     {
       sno: {
         value: 1,
-        imgSrc: null,
-        altValue: '',
         profit: 'up',
+        percent: '+2',
       }, company: {
         imgSrc: Medicine_5,
         altValue: '',
         name: 'Ocrevus',
         desc: '1L HER2+ Metastatic Colorectal Cancer (MOUNTAINEER-03)',
       }, revenue23: {
-        imgSrc: null,
-        altValue: '',
         value: '6,031',
         curr: 'Fr. CHF Million',
         profit: 'up',
+        percent: '+6',
       }, revenue22: {
         value: '6,031',
         curr: 'Fr. CHF Million',
@@ -305,20 +291,18 @@ const data = {
     {
       sno: {
         value: 2,
-        imgSrc: null,
-        altValue: '',
-        profit: 'up',
+        profit: null,
+        percent: null,
       }, company: {
         imgSrc: Medicine_5,
         altValue: '',
         name: 'Hemlibra',
         desc: 'spinal muscular atrophy (SMA) pediatric <2 months',
       }, revenue23: {
-        imgSrc: null,
-        altValue: '',
         value: '4,147',
         curr: 'Fr. CHF Million',
         profit: 'up',
+        percent: '+8',
       }, revenue22: {
         value: '3,823',
         curr: 'Fr. CHF Million',
@@ -332,20 +316,18 @@ const data = {
     }, {
       sno: {
         value: 3,
-        imgSrc: null,
-        altValue: '',
         profit: 'up',
+        percent: '-2',
       }, company: {
         imgSrc: Medicine_5,
         altValue: '',
         name: 'Perjeta',
         desc: 'Obstructive Hypertrophic Cardiomyopathy',
       }, revenue23: {
-        imgSrc: null,
-        altValue: '',
         value: '3,768',
         curr: 'Fr. CHF Million',
         profit: 'down',
+        percent: '-8',
       }, revenue22: {
         value: '4,087',
         curr: 'Fr. CHF Million',
@@ -362,20 +344,18 @@ const data = {
     {
       sno: {
         value: 1,
-        imgSrc: null,
-        altValue: '',
         profit: 'up',
+        percent: '+2',
       }, company: {
-        imgSrc: Medicine_6,
+        imgSrc: Medicine_3,
         altValue: 'Novo Nordisk',
         name: 'Ozempic',
         desc: '- Improves glycemic control in adults with type 2 diabetes mellitus.- Reduces the risk of major adverse cardiovascular events in adults with type 2 diabetes mellitus and established cardiovascular disease.- Not for use in patients with type 1 diabetes mellitus or a history of pancreatitis.',
       }, revenue23: {
-        imgSrc: null,
-        altValue: '',
         value: '95,718',
         curr: 'kr. DKK Million',
         profit: 'up',
+        percent: '+66',
       }, revenue22: {
         value: '57,661',
         curr: 'kr. DKK Million',
@@ -390,20 +370,18 @@ const data = {
     {
       sno: {
         value: 2,
-        imgSrc: null,
-        altValue: '',
-        profit: 'up',
+        profit: null,
+        percent: null,
       }, company: {
-        imgSrc: Medicine_6,
+        imgSrc: Medicine_3,
         altValue: 'Novo Nordisk',
         name: 'Wegovy',
         desc: '- Reduce risk of major adverse cardiovascular events in adults with established cardiovascular disease and either obesity or overweight.- Reduce excess body weight and maintain weight reduction long term in adults and pediatric patients aged 12 years and older with obesity.- Reduce excess body weight and maintain weight reduction long term in adults with overweight in the presence of at least one weight related comorbid condition.',
       }, revenue23: {
-        imgSrc: null,
-        altValue: '',
         value: '31,343',
         curr: 'kr. DKK Million',
         profit: 'up',
+        percent: '+420',
       }, revenue22: {
         value: '6,028',
         curr: 'kr. DKK Million',
@@ -420,20 +398,18 @@ const data = {
     {
       sno: {
         value: 1,
-        imgSrc: null,
-        altValue: '',
-        profit: '',
+        profit: null,
+        percent: null,
       }, company: {
         imgSrc: Medicine_2,
         altValue: '',
         name: 'Trulicity',
         desc: '- Improve glycemic control in adults and pediatric patients with type 2 diabetes mellitus.- Reduce the risk of major adverse cardiovascular events in adults with type 2 diabetes mellitus with established cardiovascular disease or multiple cardiovascular risk factors.',
       }, revenue23: {
-        imgSrc: null,
-        altValue: '',
         value: '7,132',
         curr: '$ Dollar Million',
         profit: 'down',
+        percent: '-4',
       }, revenue22: {
         value: '7,439',
         curr: '$ Dollar Million',
@@ -448,21 +424,19 @@ const data = {
     {
       sno: {
         value: 2,
-        imgSrc: null,
-        altValue: '',
-        profit: 'up',
+        profit: null,
+        percent: null,
       }, company: {
-        imgSrc: Medicine_6,
+        imgSrc: Medicine_2,
         altValue: '',
         name: 'Bristol Myers Suibb',
         name2: 'Eliquis',
         desc: 'Secondary Stroke Prevention',
       }, revenue23: {
-        imgSrc: null,
-        altValue: '',
         value: '5,163',
         curr: '$ Dollar Million',
         profit: 'up',
+        percent: '+970',
       }, revenue22: {
         value: '482',
         curr: '$ Dollar Million',
@@ -481,13 +455,13 @@ const data = {
 // CustomTable component
 const CustomTable = ({ selectedOption, searchValue }) => {
 
-  const temp=data[selectedOption.name];
+  const temp = data[selectedOption.name];
   console.log(temp);
-  const [rows,setRows]=useState(temp);
+  const [rows, setRows] = useState(temp);
 
-  useEffect(()=>{
+  useEffect(() => {
     setRows(data[selectedOption.name]);
-  },[selectedOption])
+  }, [selectedOption])
 
   useEffect(() => {
     console.log("First time it is working");
@@ -495,8 +469,8 @@ const CustomTable = ({ selectedOption, searchValue }) => {
       console.log(item);
       return (item.company.name.toLowerCase().includes(searchValue) ||
         item.company.desc.toLowerCase().includes(searchValue) ||
-        item.company.altValue.toLowerCase().includes(searchValue)||
-        item.revenue23.value.toLowerCase().includes(searchValue)||
+        item.company.altValue.toLowerCase().includes(searchValue) ||
+        item.revenue23.value.toLowerCase().includes(searchValue) ||
         item.revenue22.value.toLowerCase().includes(searchValue)
       )
     }
@@ -515,22 +489,30 @@ const CustomTable = ({ selectedOption, searchValue }) => {
 
   return (
     <TableContainer component={Paper}>
-      <Table sx={{ width: '1118px', tableLayout: 'fixed' }} aria-label="customized table">
+      <Table sx={{ width: '1118px', tableLayout: 'fixed', borderRadius: '10px' }} aria-label="customized table">
         <TableHead>
           <TableRow>
             <StyledTableCell sx={{ width: '80px', height: '46px' }}>S. No</StyledTableCell> {/* Serial number column */}
-            <StyledTableCell sx={{ width: '326px', height: '46px' }}>Company (Innovator)</StyledTableCell>
-            <StyledTableCell align="left" sx={{ width: '161px', height: '46px' }}>2023 Revenue ($M)</StyledTableCell>
-            <StyledTableCell align="left" sx={{ width: '161px', height: '46px' }}>2022 Revenue ($M)</StyledTableCell>
-            <StyledTableCell align="left" sx={{ backgroundColor: '#7CC8FD !important', width: '300px', height: '46px' }}>R&D pipeline - Approved</StyledTableCell>
-            <StyledTableCell align="left" sx={{ backgroundColor: '#B9E0FE !important', width: '300px', height: '46px' }}>R&D pipeline</StyledTableCell>
+            <StyledTableCell sx={{ width: '326px', height: '46px'}}>Company (Innovator)</StyledTableCell>
+            <StyledTableCell  sx={{ width: '161px', height: '46px' }}>2023 Revenue ($M)</StyledTableCell>
+            <StyledTableCell  sx={{ width: '161px', height: '46px'}}>2022 Revenue ($M)</StyledTableCell>
+            <StyledTableCell sx={{ backgroundColor: '#7CC8FD !important', width: '300px', height: '46px' }}>R&D pipeline - Approved</StyledTableCell>
+            <StyledTableCell  sx={{ backgroundColor: '#B9E0FE !important', width: '300px', height: '46px'}}>R&D pipeline</StyledTableCell>
           </TableRow>
         </TableHead>
         <TableBody>
           {rows && rows.map((row, index) => (
             <StyledTableRow key={row.name}>
-              <StyledTableCell component="th" scope="row">
-                {row.sno.value}
+              <StyledTableCell sx={{
+                backgroundColor:
+                  row?.sno?.profit === 'up' ? '#E3F5E3 !important' :
+                    row?.sno?.profit === 'down' ? '#FFF1F1 !important' : 'inherit'
+              }} component="th" scope="row">
+                <div>{row.sno.value}</div>
+                {row?.sno?.percent && (
+                    row?.sno?.percent[0] === '+' ? <div className='flex gap-[4px] text-[#067647]'><img src={UpArrow} alt='up arrow' /><div className='font-[700]'>{`(${row?.sno?.percent})`}</div></div> : <div className='flex gap-[4px] text-[#FF4C4C]'><img src={DownArroww} alt='up arrow' /><div className='font-[700]'>{`(${row?.sno?.percent})`}</div></div>
+                  )
+                  }
               </StyledTableCell>
               <StyledTableCell>
                 {row?.company?.imgSrc && <img src={row?.company?.imgSrc} alt={row?.company.altValue} style={{ width: '50px', height: '50px' }} />}
@@ -545,17 +527,21 @@ const CustomTable = ({ selectedOption, searchValue }) => {
                 }}
                 align="left"
               >
-                <div>
+                <div className='flex gap-[8px]'>
                   <div>{row?.revenue23?.value}</div>
-                  {row?.revenue23?.imgSrc && <img src={row?.revenue23?.imgSrc} style={{ width: '20px', height: '20px' }} alt="Revenue icon" />}
+
+                  {row?.revenue23?.percent && (
+                    row?.revenue23?.percent[0] === '+' ? <div className='flex gap-[4px] text-[#067647]'><img src={UpArrow} alt='up arrow' /><div className='font-[700]'>{`(${row?.revenue23?.percent})%`}</div></div> : <div className='flex gap-[4px] text-[#FF4C4C]'><img src={DownArroww} alt='up arrow' /><div className='font-[700]'>{`(${row?.revenue23?.percent})%`}</div></div>
+                  )
+                  }
                 </div>
-                <div className='w-[101px] h-[24px] bg-[#FCFBFF] rounded-[20px] border text-[11px] py-[4px] px-[10px] leading-[16.2px]'>{row.revenue23.curr}</div>
+                <div className='w-[101px] h-[24px] bg-[#FCFBFF] mt-2 rounded-[20px] border text-[11px] py-[4px] px-[10px] leading-[16.2px]'>{row.revenue23.curr}</div>
               </StyledTableCell>
               <StyledTableCell sx={{ backgroundColor: 'lightblue' }} align="left">
                 <div>
                   <div>{row?.revenue22?.value}</div>
                 </div>
-                <div className='w-[101px] h-[24px] bg-[#FCFBFF] rounded-[20px] border text-[11px] py-[4px] px-[10px] leading-[16.2px]'>{row?.revenue22?.curr}</div>
+                <div className='w-[101px] h-[24px] bg-[#FCFBFF] mt-2 rounded-[20px] border text-[11px] py-[4px] px-[10px] leading-[16.2px]'>{row?.revenue22?.curr}</div>
               </StyledTableCell>
               <StyledTableCell align="left">
                 {row?.approved?.imgSrc && <img src={row?.approved?.imgSrc} alt={row?.approved?.altValue} style={{ width: '20px', height: '20px' }} />}
--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -6,12 +6,12 @@ const SearchBar = ({searchValue, setSearchValue}) => {
     setSearchValue(e.target.value);
   }
   return (
-      <div className="flex items-center w-[471px] h-[52px] border border-[#E4E4E4] rounded-full px-4">
-        <img src={SearchIcon} alt="Search Icon" className="h-4 w-4 mr-2" /> 
+      <div className="flex items-center w-[471px] h-[52px] border border-[#E4E4E4] rounded-full py-2 px-4 gap-[10px]">
+        <img src={SearchIcon} alt="Search Icon" className="w-4 " /> 
         <input
           type="text"
           placeholder="Search drug name"
-          className="flex-grow h-full border-none outline-none placeholder:text-gray-400"
+          className="flex-grow h-full border-none outline-none placeholder:text-gray-600 "
           onChange={handleSearch}
         />
       </div>
